416807365647eb3bf71b11170a7060eb3c3deb42
이근휘 <dlrmsgnl620@naver.com>
2023-12-02 20:16:13 +0900

    Feat: display header

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,15 +1,55 @@
-import { useReactTable } from '@tanstack/react-table';
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import { useState } from 'react';
 
-const columns = [];
+import DATA from '@/data';
+
+const columns = [
+  {
+    accessorKey: 'header',
+    header: 'Task',
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: 'due',
+    header: 'Due',
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: 'task',
+    header: 'Task',
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+];
 
 export const Table: React.FC = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(DATA);
 
-  // TODO: docs와 연결해서 설명
   const table = useReactTable({
     data,
-    column,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
   });
-  return <div></div>;
+
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>{header.column.columnDef.header}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+    </table>
+  );
 };
